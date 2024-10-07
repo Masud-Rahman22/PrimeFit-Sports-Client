665245381070c680581faf7ec1318424224ea5da
Masud-Rahman22 <masudrhmn6560@gmail.com>
2024-10-07 17:00:08 +0600

    customized the banner section

--- src/components/ui/Banner.tsx
+++ src/components/ui/Banner.tsx
@@ -5,10 +5,10 @@ import './banner.css';
 const Banner: React.FC = () => (
   <Carousel autoplay className='carousel'>
     <div>
-      <img src="/src/assets/images/banner/banner1.png" alt="" className="banner-image" />
+      <img src="/src/assets/images/banner/banner2.png" alt="" className="banner-image" />
     </div>
     <div>
-      <img src="/src/assets/images/banner/banner2.webp" alt="" className="banner-image" />
+      <img src="/src/assets/images/banner/banner1.png" alt="" className="banner-image" />
     </div>
   </Carousel>
 );
